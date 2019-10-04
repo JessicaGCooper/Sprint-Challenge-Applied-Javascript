8fcbf2cc6cb723bac47fade1fcfc0b5141c9a81d
Jessica Cooper <jessicagcooper@gmail.com>
2019-10-04 11:32:00 -0500

    Finished tabs portion of assignment.

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,29 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+const topics = document.querySelector('.topics');
+
+function tabs(topic){
+    const tab = document.createElement('div');
+    tab.classList.add('tab');
+    tab.textContent = topic;
+    
+    return tab
+}
+
+axios
+.get('https://lambda-times-backend.herokuapp.com/topics')
+  .then(response => {
+    console.log(response.data.topics);
+    response.data.topics.forEach((element) => {
+      const newTab = tabs(element);
+      topics.appendChild(newTab);
+    })
+  })
+  .catch(error => {
+    console.log("The data was not returned", error);
+  })
+
+
+
